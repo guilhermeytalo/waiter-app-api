16bffcd4e2bdfeb051de9e9b2eb1e607857b591b
Guilherme Ytalo <gydaytalo@gmail.com>
2022-11-14 20:18:16 -0300

    refactor + added mongo+mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
-
-app.listen(port, () => {
-  console.log(`🏠 Server is runing on http://localhost:${port}`);
-});
-
+    app.listen(port, () => {
+      console.log(`🏠 Server is runing on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Error in conecction'));
